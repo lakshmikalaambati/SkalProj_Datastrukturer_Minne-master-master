123538db6ec8630054b1e193485715274da43efd
Lakshmi kalavathi <56877693+lakshmikalaambati@users.noreply.github.com>
2019-11-05 15:46:14 +0100

    Update Program.cs

--- skalproj_datastrukturer_minne/Program.cs
+++ skalproj_datastrukturer_minne/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -76,12 +76,93 @@ namespace SkalProj_Datastrukturer_Minne
              * Below you can see some inspirational code to begin working.
             */
 
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
-            //char nav = input[0];
-            //string value = input.substring(1);
+            List<string> theList = new List<string>();
+            PrintCapacityCountListDetails(theList);
+             
 
-            //switch(nav){...}
+            bool loopexit = true;
+            Console.WriteLine("\nDo you want to add "
+                    + "\n+NAME  Add to List"
+                    + "\n-NAME  Remove from List"
+                    + "\nq or Q  to exit");
+            do
+            {
+                
+                string input = Console.ReadLine();
+                char nav = input[0];
+                string value = input.Substring(1).Replace("\"",string.Empty);
+
+                switch(nav)
+                {
+                    case '+':
+                       if ( String.IsNullOrEmpty(value))
+	                   {
+                            Console.WriteLine("input value should not be null or empty");
+                       }
+	                   else 
+                       {
+                            theList.Add(value);
+                            PrintCapacityCountListDetails(theList);
+                       }
+                                            
+                       break;
+                    case '-':
+                        //check if value exists:
+
+                        if(theList.Contains(value))
+                        {
+                            theList.Remove(value);
+                            PrintCapacityCountListDetails(theList);
+                            break;
+                        }
+                        else
+                            Console.WriteLine("enter valid name from the list to remove");
+                            break;
+                    case 'q':
+                    case 'Q':    
+                        loopexit=false;
+                        break;
+                    default:
+                        Console.WriteLine("Invaid Input...Please enter either + to add the list or - to remove from list");
+                        break;
+                }
+            }while(loopexit);
+
+
+            //////////////////////////////////////////////////////////////////////////////////////////////
+
+            //1.​​In your words, how does the stack and the heap work in terms of structurallystoring data? You are welcome to use examples or draw to explain their basicstructures.
+
+            //2.​​What are are ​Value Types ​and ​Reference Types, ​and how do they differ?3.Following methods return different answers (see fig below). The first methodreturns
+
+            //3, the second returns
+            
+            //4. Why? ​MyInt ​is the same class that is declared inexample 2 above
+
+            ///////////////////////////////////////////////////////////////////////////////////////////
+
+            //2.When does the capacity of the list increase? (In other words the size of theunderlying array)?
+            //A: If Count exceeds Capacity while adding elements, the capacity is increased by automatically 
+            //   reallocating the internal array before copying the old elements and adding the new elements.
+
+            //3.​​How much does the capacity increase?
+            // it depends on the number of new elements added to the list.usually twice the size of the existing one.
+
+
+            //4.​​Why does the capacity not increase at the same pace as elements are added?
+            //when elements are added more than the capacity, it Creates a new array, twice the size of the old one.
+            //Copies each entry from the old array to the new one,
+            //Sets the "last" entry in the new array to the new value, and moves the "last" indicator on by one.
+
+
+
+            //5.​​Does the capacity decrease when elements are removed from the list?
+            //A: No the Capavity will not be decreased for the list even if elements were removed from the list
+
+            //6.When is it preferable to use an ​array​ you declare yourself instead of a list?
+            //A: when we know exact size of the elements coming it is better to use array instead of list.
+                                                                                                                                                                                                                                                                                         6.​​When is it preferable to use an ​array​ you declare yourself instead of a list?
+
         }
 
         /// <summary>
@@ -118,5 +199,28 @@ namespace SkalProj_Datastrukturer_Minne
 
         }
 
+
+        static void PrintCapacityCountListDetails(List<string> theList)
+        {
+            // Printing the Capacity of thelist 
+            Console.WriteLine("List is having Capacity Is: " + theList.Capacity); 
+  
+            // Printing the Count of thelist 
+            Console.WriteLine("List is having Count Is: " + theList.Count); 
+
+            Console.WriteLine("\nLIST DETAILS" );
+            if (theList.Count==0)
+	                {  Console.WriteLine("List is Empty"); }
+                else
+                {  foreach (var item in theList)
+	                {
+               
+                            {Console.WriteLine(item);}
+
+	                }
+                    Console.WriteLine(); 
+                }
+               
+        }
     }
 }
